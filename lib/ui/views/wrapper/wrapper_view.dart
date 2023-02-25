17417d6135e7e47aa9eb0ea2473d6c84d2bb637f
Dagem D Worku <dagemdworku@gmail.com>
2023-02-25 16:36:30 +0300

    wrapper view bottom navigation bar

--- lib/ui/views/wrapper/wrapper_view.dart
+++ lib/ui/views/wrapper/wrapper_view.dart
@@ -1,4 +1,7 @@
+import 'package:animations/animations.dart';
 import 'package:breath/ui/views/views.dart';
+import 'package:breath/ui/widgets/widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
@@ -10,10 +13,36 @@ class WrapperView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<WrapperViewModel>.reactive(
-      builder: (context, model, child) => const Scaffold(
-        body: HomeView(),
+      builder: (context, model, child) => Scaffold(
+        body: BPageTransition(
+          reverse: model.reverse,
+          transitionType: SharedAxisTransitionType.horizontal,
+          child: _selectView(model),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: model.setViewIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
       ),
       viewModelBuilder: () => WrapperViewModel(),
     );
+  }
+
+  Widget _selectView(WrapperViewModel model) {
+    switch (model.currentPage) {
+      case CurrentPage.home:
+        return const HomeView();
+      case CurrentPage.settings:
+        return const SettingsView();
+    }
   }
 }
