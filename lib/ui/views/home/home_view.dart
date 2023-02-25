abc422657eab9f60a53856d9384a0d1855de5cf5
Dagem D Worku <dagemdworku@gmail.com>
2023-02-25 16:16:23 +0300

    home and setting views

---
+++ lib/ui/views/home/home_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'home_viewmodel.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<HomeViewModel>.reactive(
+      builder: (context, model, child) => const Scaffold(
+        body: Center(
+          child: Text(
+            'this is home',
+          ),
+        ),
+      ),
+      viewModelBuilder: () => HomeViewModel(),
+    );
+  }
+}
---
+++ lib/ui/views/settings/settings_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'settings_viewmodel.dart';
+
+class SettingsView extends StatelessWidget {
+  const SettingsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<SettingsViewModel>.reactive(
+      builder: (context, model, child) => const Scaffold(
+        body: Center(
+          child: Text(
+            'this is settings',
+          ),
+        ),
+      ),
+      viewModelBuilder: () => SettingsViewModel(),
+    );
+  }
+}
